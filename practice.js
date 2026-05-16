a19e40dcca29563c90c7ab0c441dd0fe6d3c2b6a
Priyanshi Vishwakarma <priyanshi.cs241253@global.org.in>
2026-05-16 20:56:52 +0530

    basic questions of js

---
+++ practice.js
@@ -0,0 +1,86 @@
+// practice queations
+
+// let a = 10;
+// let b = 20;
+// if (a> 5 && b< 25) {
+//     console.log("A ")
+// }
+// else{
+//     console.log("B")          
+// }                                   
+//                            //output A
+
+
+//  let a = true;
+//  let b = false;
+//  if (a || b) {
+//     console.log(" A")
+//  }     
+//  else{
+//     console.log("B")
+//  }
+//                     //output A 
+
+// let temperature = 30;
+// if (!(temperature<25)){
+//     console.log("Hot")
+// }
+// else{
+//     console.log("pleasent")
+// }
+//                     //output hot
+
+
+// let a = 0;
+// if (a){
+//     console.log("truthy value")
+// }
+// else{
+//     console.log("falsy value ")
+// }
+//                     //output falsy value 
+                    
+                    
+//  let a =5;
+//  a++;
+//     console.log(a)  
+//                         //output 6
+
+
+// let x =3;
+// let y = x++;
+// console.log(x)  // output 4
+// console.log(y)  // output 3
+
+
+// let p = 4;
+// let q = ++p;
+// console.log(p,q)   // output 5 5
+
+
+// let n = 5;
+// n = n++ + ++n + n++ + ++n;
+// console.log(n)
+                      // output 28
+
+// let n=5;
+// n = n++ + ++n;
+// console.log(n)  
+                        // output 12
+
+// let n =100;
+// function test(){
+//     return ++n;
+// }
+// console.log(test())
+// console.log(n)  
+                        // output 101 101
+let x =5;
+if (x++ ===5) {
+    console.log("matched ")
+}  
+else{
+    console.log("not matched")
+}                     
+
+                           matched 
