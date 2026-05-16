0eac2d98097c196a7765992c95ff4638576fc96c
vishwakarmapriyanshi239-crypto <vishwakarmapriyanshi239@gmail.com>
2026-05-16 17:36:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+
+// variables in java script 
+
+// var x = 1; //global
+// {
+//     var x =2; //global
+// }
+// console.log(x);               // output: 2
+
+
+// let a =10;
+// {
+//     let a = 20;
+//     console.log("inside block : ",a);
+// }
+// console.log("outside block : ",a);
+
+// let obj = {
+//     uid: 1,
+//     name: "priyanshi",
+//     age:20,
+//     email:"priyanshi@123.com"
+// };
+// let u1 = Symbol("uid");
+// obj[u1] = 2;
+
+
+// symbol in java script 
+
+
+//  let u1 = Symbol("uid");
+//  let u2 = Symbol("uid");
+                            //(u1 === u2);  output: false
+
+// type coversion java script - one of the type is automatically changed to another type 
+
+5 + "5"; // output: "55" (number is converted to string)
+"5" * 2; // output: 10 (string is converted to number)
+true + 1; // output: 2 (boolean is converted to number)
+
+
