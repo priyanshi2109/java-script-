82a6095e1a4c855dbbcc660e09a69ef7e3913819
Priyanshi Vishwakarma <priyanshi.cs241253@global.org.in>
2026-05-16 21:36:15 +0530

    js basic question

---
+++ practice2.js
@@ -0,0 +1,44 @@
+// var x = 10;
+// {
+//   var x = 20;
+// }
+// console.log(x);  // output 20
+
+
+// let x = 10;
+// {
+//     let x = 20 ;
+
+// }
+// console.log(x)  // output 10
+
+
+// const a = 5;
+// a = 10;
+// console.log(a);  // output error because we cannot reassign a value to a constant variable
+
+
+// let x = 5;
+// console.log(x++);  // output 5 
+
+// let x = 5;
+// console.log(++x);   //output 6 
+
+// let x = 10;
+// console.log(x--); // output 10
+
+//  let x = 10;
+// console.log(--x);  // output 9
+
+// let a = 5;
+// let b = a++;
+// console.log(a, b);  // output 6 5
+
+// let x = 3;
+// let y = x++ + ++x;
+// console.log(y); // output 8 = 3 +5
+
+//  let x = 2;
+// x++;
+// x++;
+// console.log(x);   // output 4 
