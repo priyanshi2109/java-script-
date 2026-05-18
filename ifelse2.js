548f2fa2eb8c3e768527cf99178a597de09ebeb6
Priyanshi Vishwakarma <priyanshi.cs241253@global.org.in>
2026-05-18 20:15:41 +0530

    if else q 2

---
+++ ifelse2.js
@@ -0,0 +1,13 @@
+
+
+// 1. Write a program to check if the number is positive, negative or zero. 
+let num = Number(prompt("Enter a number:"));
+
+if (num > 0) {
+    console.log("Positive number");
+} else if (num < 0) {
+    console.log("Negative number");
+} else {
+    console.log("Zero");
+}
+
