b03b6a03e5d1b6ba0f2499c5c1998a2d6ad865bd
Priyanshi Vishwakarma <priyanshi.cs241253@global.org.in>
2026-05-22 11:44:41 +0530

    while and do while loop basic statement

---
+++ while&dwhile.js
@@ -0,0 +1,17 @@
+// write fromm 1 untill 32 comes using while loop
+
+let i = 1
+while(i<33){
+    console.log(i);
+    i++;
+}
+
+
+// dowhile loop= ek bar jaroor excute hoga bina condition check kiye hue
+
+// let j = 1;
+// do {
+//     console.log(j);
+//     j++;
+// } while (j < 33);
+
