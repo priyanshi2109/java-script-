17ba67e98bc2e8bdaccbf7f70844df3af02711f3
Priyanshi Vishwakarma <priyanshi.cs241253@global.org.in>
2026-05-22 11:43:00 +0530

    for loop basic idea

---
+++ loops.js
@@ -0,0 +1,16 @@
+//LOOPS 
+// for loop
+// print 1 to 100 using for loop
+
+
+// for(let i =1;i<101;i++){
+//     console.log(i);
+// }
+
+// print table of 5 using for loop
+
+
+let i=5;
+for (let i =5;i<51;i+=5){
+    console.log(i);
+}
