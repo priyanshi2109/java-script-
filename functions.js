78bd3c1695249401833e6ceee9c20d6fda25f8a6
Priyanshi Vishwakarma <priyanshi.cs241253@global.org.in>
2026-05-24 10:27:21 +0530

    functions in js

---
+++ functions.js
@@ -0,0 +1,30 @@
+// functions 
+
+// structure of functions 
+
+ // function functionname(parameters){
+ //    console.log("hello world ");
+ //}
+
+// function expression
+
+//  let fnc=function(){
+//     console.log("heyy thids is priyanshi")
+//  }
+// fnc();
+
+// fat arrow function
+
+// let func=()=>{
+//     console.log("this is fat arrow function")
+// }
+// func();
+
+// parameters and arguments 
+
+function sum(a,b){
+    console.log(a+b);
+}
+sum(5,10);
+sum(20,30); // output will be 15 and 50 respectively
+
