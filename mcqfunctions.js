0ed57fc6949138972c6a7d232e4b7e63a8cd81cc
Priyanshi Vishwakarma <priyanshi.cs241253@global.org.in>
2026-05-24 10:28:47 +0530

    mcq of functions

---
+++ mcqfunctions.js
@@ -0,0 +1,57 @@
+// mcq questions of functions 
+
+// function greet(name) {
+//   return "Hello " + name;
+// }
+
+// console.log(greet());   // Output: "Hello undefined"
+
+
+
+
+// function add(a, b) {
+//   console.log(a + b);
+// }
+
+// add(4, "5");  // Output: "45" (string concatenation)
+
+
+
+
+
+// function test() {
+//   return;
+// }
+
+// console.log(test()); // Output: undefined
+
+
+
+
+
+// function multiply(a, b = 2) {
+//   return a * b;
+// }
+
+// console.log(multiply(5)); // Output: 10 (5 multiplied by the default value of 2)
+
+
+
+
+
+// function show(a, b) {
+//   return a;
+// }
+// console.log(show(10, 20)); // Output: 10 (the first argument is returned, the second argument is ignored)
+
+
+
+
+
+function hello(name) {
+  console.log("Hi " + name);
+}
+
+hello; //  no Output:
+
+
