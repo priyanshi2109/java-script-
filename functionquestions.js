7e639480559edcfce2a1a96b5161bc1acf6628d3
Priyanshi Vishwakarma <priyanshi.cs241253@global.org.in>
2026-06-04 21:26:45 +0530

    some functions questions

---
+++ functionquestions.js
@@ -0,0 +1,51 @@
+// function questions 
+
+
+ // check if the number is even or odd 
+
+// function EvenorOdd(num){
+//     if(num%2===0){ 
+//         return" even";
+//     } 
+//     else{
+//         return "odd";
+//     }
+// }
+//  console.log(EvenorOdd(2))
+
+
+// give the square of 4
+
+// function square(val){
+//     return val*val;
+// }
+// console.log(square(4))
+
+
+// find the largest number of two number taking input
+
+// function maximum(val1,val2){
+//  if(val1>val2){
+//     return val1;
+//  }
+//  else {
+//     return val2;
+//  }
+// }
+// // console.log(maximum(5,6))
+// console.log(maximum(19,9))
+
+// convert this function into arrow function '
+
+// function multiply(val1,val2){
+//     return val1*val2;
+
+// }
+// console.log(multiply(5,6))
+
+// arrow function
+
+const multiply=(val1,val2)=>{
+    return val1*val2;
+}
+console.log(multiply(5,6))
