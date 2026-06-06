a25cca84064d8f3adac56f7a1618ae2b3466b9dc
Priyanshi Vishwakarma <priyanshi.cs241253@global.org.in>
2026-06-06 21:40:40 +0530

    level 2 arrays questions

--- arraysques.js
+++ arraysques.js
@@ -1,39 +1,38 @@
-// print the array of fruits and also print the second fruit 
-
-// let fruits = [ "apple","banana","graps"]
-// console.log(fruitjs[1]) // [ "apple","banana","graps"]
-
-// add a new fruit at the begining and end of the array
 
 
-// fruits.push("orange") // add at the end
-// fruits.unshift("mango") // add at the begining
-// console.log(fruits) // [ "mango","apple","banana","graps","orange"]
 
-// replace banana with strawberry
- 
-// fruits[2] = "strawberry"
-// console.log(fruits) // [ "mango","apple","strawberry","graps","orange" ]
+// level 2 arrays questions loops +arrays
 
 
-//insert a new two fruits mango and kiwi at index 1 
+// print the all elements of the given array using for loop
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+// for(let i=0;i<arr.length;i++){
+//     console.log(arr[i]) // 1 2 3 4 5 6 7 8 9 10
+// }
 
-// fruits.splice(1,0,"mango","kiwi") // add at index 1 // 1,0, means no element will be removed form the index 1 but two elements will be added
-// console.log(fruits) // [ "mango","mango","kiwi","apple","strawberry","graps","orange" ]
+// print all elements with multiply by 2
+
+// for(let i=0;i<arr.length;i++){
+//     console.log(arr[i]*2) // 2 4 6 8 10 12 14 16 18 20
+// }
+
+// find the sum of all elements in the array
+
+// let arr =[10,20,30]
+// let sum=0;
+// for(let i =0;i<arr.length;i++){
+//     sum = sum +arr[i]
+// }
+// console.log(sum) // 60
+
+// count the number of elements in array
 
 
-// extract the value of 2 middle values from the array
+let arr = [1,2,3,45,6,7]
+let count =0;
+for(let i=0;i<arr.length;i++){
+    count++;
+}
+console.log(count) // 6
 
-// let arr = [1,2,3,4,5,6]
-//  let newarr = arr.slice(2,4) // [3,4] // 2 is the starting index and 4 is the ending index but the element at index 4 will not be included in the result
-//     console.log(newarr) // [3,4]
-//     console.log(arr) // [1,2,3,4,5,6] // original array is not modified
-
-
-// use map to square the value of each element in the array
-let arr = [1,2,3,4,5]
-let squaredArr = arr.map(function(val){
-    return val*val;
-})
-console.log(squaredArr) // [1,4,9,16,25]
 
